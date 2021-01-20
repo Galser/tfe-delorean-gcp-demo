f863c09867b03c8867296872e174d25efcf7ebf2
Andrii Guselietov <guseletov@gmail.com>
2021-01-20 11:29:08 +0100

    Instance group

---
+++ modules/compute_instance_group/variables.tf
@@ -0,0 +1,15 @@
+# Compute Instance Group (GCP) module variables
+variable "name" {
+  description = "Name of the project for your tfe installation"
+  type        = string
+}
+
+variable "instances" {
+  description = "List of instances to attach into group"
+  type        = list
+  default     = []
+}
+
+variable "availabilityZone" {
+  type        = string
+}
